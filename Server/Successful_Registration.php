5d7a13fd5709ea09e20caef6554025345f71271b
MKsokili <mksokili20@gmail.com>
2023-06-12 23:49:25 +0100

    updated one

---
+++ Server/Successful_Registration.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Signup</title>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css">
+        <link rel="stylesheet" href="../css/Successful_Registration.css">
+    </head>
+
+    <body>
+        <div class="suc">
+        <h1>Signup</h1>
+            <p> Now we can Tell you officially  Welcome to our website dedicated to internships and job opportunities! Explore a wide range of exciting professional opportunities in various fields and industries. Discover internships and job listings that align with your skills and career aspirations. and also where you can create your own offers
+               <br>
+               <a href="Login.php">log in</a>.
+            </p>
+        </div>
+        <img src="../assets/desk.jpg">
+    </body>
+</html>
